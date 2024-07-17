4f44c9217c389c93dafa7bf0d5c38e0d6d756943
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2024-07-16 17:49:27 -0700

    Update toplevel.vhd

--- toplevel.vhd
+++ toplevel.vhd
@@ -3,10 +3,8 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY toplevel IS
-    PORT(clk, RST, det : IN STD_LOGIC;
-         RX : IN STD_LOGIC_VECTOR (8 DOWNTO 0);
-         led : OUT STD_LOGIC;
-         TX : OUT STD_LOGIC_VECTOR (9 DOWNTO 0);
+    PORT(clk, RST, det, RX : IN STD_LOGIC;
+         led, TX : OUT STD_LOGIC;
          CMD : INOUT STD_LOGIC;
          DAT : INOUT STD_LOGIC_VECTOR (3 DOWNTO 0)
     );
@@ -15,7 +13,7 @@ END ENTITY;
 ARCHITECTURE behavior OF toplevel IS
 
 SIGNAL oclk : STD_LOGIC;
-SIGNAL busy : STD_LOGIC;
+SIGNAL triggerRX : STD_LOGIC;
 
 COMPONENT SDDET IS
     PORT(DET : IN STD_LOGIC;
@@ -29,22 +27,20 @@ COMPONENT clockdiv IS
 END COMPONENT;
 
 COMPONENT UARTRX IS
-    PORT(baud : IN STD_LOGIC;
-         busy : OUT STD_LOGIC;
-         data : IN STD_LOGIC_VECTOR (8 DOWNTO 0)
+    PORT(baud, dataRX : IN STD_LOGIC;
+         triggerRX : OUT STD_LOGIC
          );
 END COMPONENT;
 
 COMPONENT UARTTX IS
-    PORT(baud : IN STD_LOGIC;
-         busy : OUT STD_LOGIC;
-         data : OUT STD_LOGIC_VECTOR (9 DOWNTO 0)
+    PORT(baud, triggerTX : IN STD_LOGIC;
+         dataTX : OUT STD_LOGIC
          );
 END COMPONENT;
 
 BEGIN
-    CARD : SDDET PORT MAP (det => DET, led => LED);
-    div : clockdiv PORT MAP (clk => iclk, RST => clr, oclk => oclk);
-    uart_rx : UARTRX PORT MAP (oclk => baud, busy => busy, RX => data);
-    uart_tx : UARTTX PORT MAP (oclk => baud, busy => busy, TX => data);
+    CARD : SDDET PORT MAP (DET => det, LED => led);
+    div : clockdiv PORT MAP (iclk => clk, clr => RST, oclk => oclk);
+    uart_rx : UARTRX PORT MAP (baud => oclk, dataRX => RX, triggerRX => triggerRX);
+    uart_tx : UARTTX PORT MAP (baud => oclk, triggerTX => triggerRX, dataTX => TX);
 END ARCHITECTURE;
