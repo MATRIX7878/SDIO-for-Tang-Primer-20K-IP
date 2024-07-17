feed78f1ab03bedfd06737ae260dcca131bbdf97
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2024-07-16 17:49:41 -0700

    Update UART.vhd

--- UART.vhd
+++ UART.vhd
@@ -1,46 +1,85 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.NUMERIC_STD.ALL;
+USE IEEE.NUMERIC_STD_UNSIGNED.ALL;
 
-ENTITY toplevel IS
-    PORT(clk, RST, det, RX : IN STD_LOGIC;
-         led, TX : OUT STD_LOGIC;
-         CMD : INOUT STD_LOGIC;
-         DAT : INOUT STD_LOGIC_VECTOR (3 DOWNTO 0)
-    );
-END ENTITY;
-
-ARCHITECTURE behavior OF toplevel IS
-
-SIGNAL oclk : STD_LOGIC;
-SIGNAL triggerRX : STD_LOGIC;
-
-COMPONENT SDDET IS
-    PORT(DET : IN STD_LOGIC;
-         LED : OUT STD_LOGIC
-        );
-END COMPONENT;
-
-COMPONENT clockdiv IS
-    PORT(iclk, clr : IN STD_LOGIC;
-         oclk : OUT STD_LOGIC);
-END COMPONENT;
-
-COMPONENT UARTRX IS
+ENTITY UARTRX IS
     PORT(baud, dataRX : IN STD_LOGIC;
          triggerRX : OUT STD_LOGIC
          );
-END COMPONENT;
+END ENTITY;
 
-COMPONENT UARTTX IS
+ARCHITECTURE behavior OF UARTRX IS
+TYPE state IS (IDLE, START, RX, STOP);
+SIGNAL currentState, nextState : state;
+
+SIGNAL counter : INTEGER;
+
+SIGNAL receive : STD_LOGIC_VECTOR (7 DOWNTO 0);
+
+BEGIN
+    PROCESS(ALL)
+    BEGIN
+        IF RISING_EDGE(baud) THEN
+            CASE currentState IS
+            WHEN IDLE => IF dataRX = '0' THEN
+                triggerRX <= '0';
+                nextState <= START;
+            ELSE
+                nextState <= IDLE;
+            END IF;
+            WHEN START => nextState <= RX;
+            WHEN RX => IF counter = 8 THEN
+                counter <= 0;
+                nextState <= STOP;
+            ELSE
+                receive(counter) <= dataRX;
+                counter <= counter + 1;
+            END IF;
+            WHEN STOP => triggerRX <= '1';
+                nextState <= IDLE;
+            END CASE;
+        END IF;
+    END PROCESS;
+END ARCHITECTURE;
+
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD_UNSIGNED.ALL;
+
+ENTITY UARTTX IS
     PORT(baud, triggerTX : IN STD_LOGIC;
          dataTX : OUT STD_LOGIC
          );
-END COMPONENT;
+END ENTITY;
+
+ARCHITECTURE Behavior OF UARTTX IS
+TYPE state IS (IDLE, START, TX, STOP);
+SIGNAL currentState, nextState : state;
+
+SIGNAL counter : STD_LOGIC_VECTOR (2 DOWNTO 0) := (OTHERS => '0');
 
 BEGIN
-    CARD : SDDET PORT MAP (DET => det, LED => led);
-    div : clockdiv PORT MAP (iclk => clk, clr => RST, oclk => oclk);
-    uart_rx : UARTRX PORT MAP (baud => oclk, dataRX => RX, triggerRX => triggerRX);
-    uart_tx : UARTTX PORT MAP (baud => oclk, triggerTX => triggerRX, dataTX => TX);
+    PROCESS(ALL)
+    BEGIN
+        IF RISING_EDGE(baud) THEN
+            CASE currentState IS
+            WHEN IDLE => IF triggerTX THEN
+                nextState <= START;
+            ELSE
+                nextState <= IDLE;
+            END IF;
+            WHEN START => dataTX <= '0';
+                nextState <= TX;
+            WHEN TX => IF counter = 8 THEN
+                counter <= (OTHERS => '0');
+                nextState <= STOP;
+            ELSE
+                dataTX <= dataTX;
+                counter <= counter + '1';
+            END IF;
+            WHEN STOP => dataTX <= '1';
+                nextState <= IDLE;
+            END CASE;
+        END IF;
+    END PROCESS;
 END ARCHITECTURE;
