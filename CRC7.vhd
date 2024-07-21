88bb75c8a7b741bc9b52938453f64efda564cdb8
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2024-07-20 23:50:41 -0700

    Update CRC7.vhd

--- CRC7.vhd
+++ CRC7.vhd
@@ -2,25 +2,23 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY CRC7 IS
-    PORT (clk : IN STD_LOGIC;
-          CMD : IN STD_LOGIC_VECTOR (39 DOWNTO 0);
-          CRC7 : OUT STD_LOGIC_VECTOR (6 DOWNTO 0) := (OTHERS => '0')
+    PORT (clk, CLR : IN STD_LOGIC;
+          DATA : IN STD_LOGIC;
+          CRC7 : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
          );
 END ENTITY;
 
 ARCHITECTURE behavior OF CRC7 IS
 
-SIGNAL remainder : STD_LOGIC := '0';
-
-SIGNAL counter : INTEGER := 39;
+SIGNAL remainder : STD_LOGIC := DATA XOR CRC7(6);
 
 BEGIN
     PROCESS(ALL)
     BEGIN
         IF RISING_EDGE(clk) THEN
-            IF counter > -1 THEN
-                remainder <= CMD(counter) XOR CRC7(6);
-
+            IF CLR THEN
+                CRC7 <= (OTHERS => '0');
+            ELSE
                 CRC7(6) <= CRC7(5);
                 CRC7(5) <= CRC7(4);
                 CRC7(4) <= CRC7(3);
@@ -28,8 +26,6 @@ BEGIN
                 CRC7(2) <= CRC7(1);
                 CRC7(1) <= CRC7(0);
                 CRC7(0) <= remainder;
-
-                counter <= counter - 1;
             END IF;
         END IF;
     END PROCESS;
