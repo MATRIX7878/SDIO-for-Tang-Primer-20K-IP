52ff1a39a20c0c3a2139d40022acb3f3597fff78
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2024-07-23 16:31:47 -0700

    Update toplevel.vhd

--- toplevel.vhd
+++ toplevel.vhd
@@ -34,16 +34,16 @@ COMPONENT UART_RX IS
     PORT(clk : IN  STD_LOGIC;
          reset : IN  STD_LOGIC;
          rx_IN : IN  STD_LOGIC;
-         rx_data : OUT STD_LOGIC_VECTOR (7 downto 0);
-         rx_ready : OUT STD_LOGIC
+         rx_ready : OUT STD_LOGIC;
+         rx_data : OUT STD_LOGIC_VECTOR (7 downto 0)
          );
 END COMPONENT;
 
 COMPONENT UART_TX IS
     PORT(clk : IN  STD_LOGIC;
          reset : IN  STD_LOGIC;
+         tx_ready : IN STD_LOGIC;
          tx_data : IN  STD_LOGIC_VECTOR (7 downto 0);
-         tx_ready : OUT STD_LOGIC;
          tx_OUT : OUT STD_LOGIC
          );
 END COMPONENT;
@@ -67,12 +67,10 @@ COMPONENT SDCTRL IS
 END COMPONENT;
 
 BEGIN
-    tx_data <= rx_data;
-
-    CARD : SDDET PORT MAP (DET => det, LED => led);
-    uartrx : UART_RX PORT MAP (clk => clk, reset => RST, rx_in => RX, rx_data => rx_data, rx_ready => rx_ready);
-    uarttx : UART_TX PORT MAP (clk => clk, reset => RST, tx_data => tx_data, tx_ready => tx_ready, tx_OUT => TX);
+    --CARD : SDDET PORT MAP (DET => det, LED => led);
+    uartrx : UART_RX PORT MAP (clk => clk, reset => RST, rx_in => RX, rx_ready => rx_ready, rx_data => rx_data);
+    uarttx : UART_TX PORT MAP (clk => clk, reset => RST, tx_data => rx_data, tx_ready => rx_ready, tx_OUT => TX);
     TURTLE : clockSlow PORT MAP(iclk => clk, clr => RST, oslow => oslow);
     RABBIT : clockFast PORT MAP(iclk => clk, clr => RST, ofast => ofast);
-    SDINIT : SDCTRL PORT MAP (clk => clk, clkSlow => oslow, clkFast => ofast, led => led, CMD => ARGRESP, C2RESP => C2RESP);
+   -- SDINIT : SDCTRL PORT MAP (clk => clk, clkSlow => oslow, clkFast => ofast, led => led, CMD => ARGRESP, C2RESP => C2RESP);
 END ARCHITECTURE;
