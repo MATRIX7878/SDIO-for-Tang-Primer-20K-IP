56dde263a40988eada964a8b2c78323b6a50fb7c
MATRIX7878 <90746490+MATRIX7878@users.noreply.github.com>
2024-08-03 22:43:48 -0700

    Update SD.vhd

--- SD.vhd
+++ SD.vhd
@@ -43,7 +43,7 @@ COMPONENT CRC7 IS
           CRC7 : OUT STD_LOGIC_VECTOR (6 DOWNTO 0)
          );
 END COMPONENT;
-TYPE state IS (DUMMY, CMD0, CMD55, CMD55RESP, ACMD41, ACMD41RESP, CMD2, CMD2RESP, CMD3, CMD3RESP);
+TYPE state IS (DUMMY, CMD0, CMD55, CMD55RESP, ACMD41, NID41, ACMD41RESP, CMD2, NID2, CMD2RESP, CMD3, CMD3RESP);
 SIGNAL nextState, currentState : state;
 
 SIGNAL card, CLR, args : STD_LOGIC;
@@ -57,7 +57,7 @@ SIGNAL cycle7 : STD_LOGIC_VECTOR (6 DOWNTO 0);
 SIGNAL CID : STD_LOGIC_VECTOR (119 DOWNTO 0);
 SIGNAL CSD : STD_LOGIC_VECTOR (126 DOWNTO 0);
 
-SIGNAL counter : INTEGER := 0;
+SIGNAL counter, timer : INTEGER := 0;
 
 BEGIN
     det : SDDET PORT MAP(DET => card);
@@ -134,7 +134,7 @@ BEGIN
                             stop <= CMD(0);
                             nextState <= ACMD41;
                         ELSE
-                            led <= '0';
+                            led <= '1';
                             LOOP
                             END LOOP;
                         END IF;
@@ -157,8 +157,14 @@ BEGIN
                         CMD(7 DOWNTO 1) <= redun7(6 DOWNTO 0);
                         CMD(0) <= '1';
                         counter <= 47;
-                        nextState <= ACMD41RESP;
+                        nextState <= NID41;
                     END IF;
+                WHEN NID41 => IF timer = 4 THEN
+                    timer <= 0;
+                    nextState <= ACMD41RESP;
+                ELSE
+                    timer <= timer + 1;
+                END IF;
                 WHEN ACMD41RESP => start <= CMD(47);
                     trans <= CMD(46);
                     cmdind <= CMD(45 DOWNTO 40);
@@ -172,105 +178,122 @@ BEGIN
                     END IF;
                 END CASE;
             END IF;
-            IF RISING_EDGE(clkFast) THEN
-                CASE currentState IS
-                WHEN CMD2 => CMD(47) <= '0';
-                    CMD(46) <= '1';
-                    CMD(45 DOWNTO 40) <= TO_STDLOGICVECTOR(2, 6);
-                    CMD(39 DOWNTO 8) <= (OTHERS => '0'); 
-                    IF counter = 47 THEN
-                        CLR <= '1';
-                        args <= CMD(counter);
-                        counter <= counter - 1;
-                        nextState <= CMD2;
-                    ELSIF counter > -1 THEN
-                        CLR <= '0';
-                        args <= CMD(counter);
-                        counter <= counter - 1;
-                        nextState <= CMD2;
-                    ELSE
-                        CMD(7 DOWNTO 1) <= redun7(6 DOWNTO 0);
-                        CMD(0) <= '1';
-                        counter <= 135;
-                        nextState <= CMD2RESP;
-                    END IF;
-                WHEN CMD2RESP => start <= C2RESP(135);
-                    trans <= C2RESP(134);
-                    cmdind <= C2RESP(133 DOWNTO 128);
-                    CSD <= C2RESP(127 DOWNTO 1);
-                    CID <= C2RESP(127 DOWNTO 8);
-                    cycle7 <= C2RESP(7 DOWNTO 1);
-                    IF counter = 135 THEN
-                        CLR <= '1';
-                        args <= CID(counter);
-                        counter <= counter - 1;
-                        nextState <= CMD2RESP;
-                    ELSIF counter > -1 THEN
-                        CLR <= '0';
-                        args <= CID(counter);
-                        counter <= counter - 1;
-                        nextState <= CMD2RESP;
-                    ELSE
-                        counter <= 47;
-                        IF redun7 = cycle7 THEN
-                            stop <= C2RESP(0);
-                            nextState <= CMD3;
-                        ELSE
-                            led <= '0';
-                            LOOP
-                            END LOOP;
-                        END IF;
-                        stop <= C2RESP(0);
-                    END IF;
-                WHEN CMD3 => CMD(47) <= '0';
-                    CMD(46) <= '1';
-                    CMD(45 DOWNTO 40) <= TO_STDLOGICVECTOR(3, 6);
-                    CMD(39 DOWNTO 8) <= (OTHERS => '0'); 
-                    IF counter = 47 THEN
-                        CLR <= '1';
-                        args <= CMD(counter);
-                        counter <= counter - 1;
-                        nextState <= CMD3;
-                    ELSIF counter > -1 THEN
-                        CLR <= '0';
-                        args <= CMD(counter);
-                        counter <= counter - 1;
-                        nextState <= CMD3;
-                    ELSE
-                        CMD(7 DOWNTO 1) <= redun7(6 DOWNTO 0);
-                        CMD(0) <= '1';
-                        counter <= 39;
-                        nextState <= CMD3RESP;
-                    END IF;
-                WHEN CMD3RESP => start <= CMD(47);
-                    trans <= CMD(46);
-                    cmdind <= CMD(45 DOWNTO 40);
-                    RCA <= CMD(39 DOWNTO 24);
-                    STAT <= CMD(23 DOWNTO 8);
-                    cycle7 <= CMD(7 DOWNTO 1);
-                    IF counter = 47 THEN
-                        CLR <= '1';
-                        args <= CMD(counter);
-                        counter <= counter - 1;
-                        nextState <= CMD3RESP;
-                    ELSIF counter > -1 THEN
-                        CLR <= '0';
-                        args <= CMD(counter);
-                        counter <= counter - 1;
-                        nextState <= CMD3RESP;
-                    ELSE
-                        counter <= 47;
-                        IF redun7 = cycle7 THEN
-                            stop <= CMD(0);
-                            nextState <= CMD3;
-                        ELSE
-                            led <= '0';
-                            LOOP
-                            END LOOP;
-                        END IF;
-                    END IF;
-                END CASE;
+        END IF;
+    END PROCESS;
+
+    PROCESS(ALL)
+    BEGIN
+        IF RISING_EDGE(clkFast) THEN
+            CASE currentState IS
+            WHEN CMD2 => CMD(47) <= '0';
+                CMD(46) <= '1';
+                CMD(45 DOWNTO 40) <= TO_STDLOGICVECTOR(2, 6);
+                CMD(39 DOWNTO 8) <= (OTHERS => '0'); 
+                IF counter = 47 THEN
+                    CLR <= '1';
+                    args <= CMD(counter);
+                    counter <= counter - 1;
+                    nextState <= CMD2;
+                ELSIF counter > -1 THEN
+                    CLR <= '0';
+                    args <= CMD(counter);
+                    counter <= counter - 1;
+                    nextState <= CMD2;
+                ELSE
+                    CMD(7 DOWNTO 1) <= redun7(6 DOWNTO 0);
+                    CMD(0) <= '1';
+                    counter <= 135;
+                    nextState <= NID2;
+                END IF;
+            WHEN NID2 => IF timer = 4 THEN
+                timer <= 0;
+                nextState <= CMD2RESP;
+            ELSE
+                timer <= timer + 1;
             END IF;
+            WHEN CMD2RESP => start <= C2RESP(135);
+                trans <= C2RESP(134);
+                cmdind <= C2RESP(133 DOWNTO 128);
+                CSD <= C2RESP(127 DOWNTO 1);
+                CID <= C2RESP(127 DOWNTO 8);
+                cycle7 <= C2RESP(7 DOWNTO 1);
+                IF counter = 135 THEN
+                    CLR <= '1';
+                    args <= C2RESP(counter);
+                    counter <= counter - 1;
+                    nextState <= CMD2RESP;
+                ELSIF counter > -1 THEN
+                    CLR <= '0';
+                    args <= C2RESP(counter);
+                    counter <= counter - 1;
+                    nextState <= CMD2RESP;
+                ELSE
+                    counter <= 47;
+                    IF redun7 = cycle7 THEN
+                        stop <= C2RESP(0);
+                        nextState <= CMD3;
+                    ELSE
+                        led <= '1';
+                        LOOP
+                        END LOOP;
+                    END IF;
+                    stop <= C2RESP(0);
+                END IF;
+            WHEN CMD3 => CMD(47) <= '0';
+                CMD(46) <= '1';
+                CMD(45 DOWNTO 40) <= TO_STDLOGICVECTOR(3, 6);
+                CMD(39 DOWNTO 8) <= (OTHERS => '0'); 
+                IF counter = 47 THEN
+                    CLR <= '1';
+                    args <= CMD(counter);
+                    counter <= counter - 1;
+                    nextState <= CMD3;
+                ELSIF counter > -1 THEN
+                    CLR <= '0';
+                    args <= CMD(counter);
+                    counter <= counter - 1;
+                    nextState <= CMD3;
+                ELSE
+                    CMD(7 DOWNTO 1) <= redun7(6 DOWNTO 0);
+                    CMD(0) <= '1';
+                    counter <= 39;
+                    nextState <= CMD3RESP;
+                END IF;
+            WHEN CMD3RESP => start <= CMD(47);
+                trans <= CMD(46);
+                cmdind <= CMD(45 DOWNTO 40);
+                RCA <= CMD(39 DOWNTO 24);
+                STAT <= CMD(23 DOWNTO 8);
+                cycle7 <= CMD(7 DOWNTO 1);
+                IF counter = 47 THEN
+                    CLR <= '1';
+                    args <= CMD(counter);
+                    counter <= counter - 1;
+                    nextState <= CMD3RESP;
+                ELSIF counter > -1 THEN
+                    CLR <= '0';
+                    args <= CMD(counter);
+                    counter <= counter - 1;
+                    nextState <= CMD3RESP;
+                ELSE
+                    counter <= 47;
+                    IF redun7 = cycle7 THEN
+                        stop <= CMD(0);
+                        nextState <= CMD3;
+                    ELSE
+                        led <= '1';
+                        LOOP
+                        END LOOP;
+                    END IF;
+                END IF;
+            END CASE;
+        END IF;
+    END PROCESS;
+
+    PROCESS(clk, currentState)
+    BEGIN
+        IF RISING_EDGE(clk) THEN
+            nextState <= currentState;
         END IF;
     END PROCESS;
 END ARCHITECTURE;
